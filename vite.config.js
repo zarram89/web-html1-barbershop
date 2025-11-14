c916bbd61d051022b47f06fbb65999089fdef829
Ramil <zarram89@gmail.com>
2025-11-14 17:47:46 +0500

    chore: add dynamic base for GH Pages + sharp

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
 import { defineConfig, loadEnv } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import svgSprite from "vite-plugin-svg-sprite";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
     root: "src",
-
-    // автоматически берём base из .env.production
     base: env.VITE_BASE || "/",
 
     build: {
       outDir: "../dist",
       emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, "src/index.html"),
+          catalog: path.resolve(__dirname, "src/catalog.html"),
+        },
+      },
     },
 
     plugins: [
