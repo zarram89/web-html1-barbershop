a7f8d23e9c777ce753c9fb767de5f018e16dbe82
Ramil <zarram89@gmail.com>
2025-11-14 14:25:26 +0500

    feat: vite

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import svgSprite from "vite-plugin-svg-sprite";
+
+export default defineConfig({
+  root: "src",
+
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
+
+  plugins: [
+    svgSprite({
+      include: "icons/**/*.svg",
+      symbolId: file => "icon-" + file.name,
+    }),
+
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 80 },
+      webp: { quality: 80 }
+    }),
+  ],
+});
